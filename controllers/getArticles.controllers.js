e7a5f486393393422711a16a2b7eec765ce1eb8f
SamsonThompson <samsonthompson@samsontpsonsair.home>
2024-01-16 15:29:22 +0000

    resolve comments made on first pull request and some refactoring against SQL injection

--- controllers/getArticles.controllers.js
+++ controllers/getArticles.controllers.js
@@ -1,18 +1,11 @@
 const { fetchArticlesById } = require('../models/getArticles.models')
 
-exports.getArticles = (req, res, next) => {
-
-    const article_id = req.params.article_id
-    // console.log(article_id)
-    
-    fetchArticlesById(article_id)
-        .then((article) => {
-            if (article.length === 0) {
-               return Promise.reject({status: 404, message: 'Article not found'})
-            } else {
-                res.status(200).send({article : data})
-            }
-        })
+exports.getArticle = (req, res, next) => {
+    const id = req.params.article_id
+    fetchArticlesById(id)
+        .then((data) => {
+            res.status(200).send({article : data})
+            })
         .catch((err) => {
             next(err);
         })
