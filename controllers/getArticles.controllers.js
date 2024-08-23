bc307866a46f3f2ade3c87209bbba991fe7fbf8c
Sam Thompson <samson.m.thompson@gmail.com>
2024-08-23 12:24:16 +0100

    Implement sorting by topic with ASC/DESC order in articles model

--- controllers/getArticles.controllers.js
+++ controllers/getArticles.controllers.js
@@ -1,11 +1,13 @@
 const { fetchArticles } = require('../models/getArticles.models')
 
 exports.getArticles = (req, res, next) => {
-    fetchArticles()
-    .then((data) => {
-        res.status(200).send({articles : data})
-    })
-    .catch((err) => {
-        next(err)
-    })
-}
+    const { topic, order = 'ASC' } = req.query;
+    
+    fetchArticles(topic, order)
+      .then((data) => {
+        res.status(200).send({ articles: data });
+      })
+      .catch((err) => {
+        next(err);
+      });
+  };
